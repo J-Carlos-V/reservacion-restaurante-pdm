7c6faebdb61a3848ac2066bd749a461738401a56
Juan_Carlos_V <juancarlos.vivero@utxicotepec.edu.mx>
2020-10-09 15:45:59 -0500

    Se arreglo el logo del header y footer se agrego bootstrap en node cambiando los archivos a .scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { FooterComponent } from './componentes/footer/footer.component';
+import { from } from 'rxjs';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,7 @@ import { FooterComponent } from './componentes/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
