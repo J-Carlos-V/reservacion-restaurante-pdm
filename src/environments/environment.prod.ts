e89d28d6a34d08b4980208ed8fb385f2a5734803
Alejandrom-GO <miguel.a.gutierrezortega@gmail.com>
2020-10-16 16:52:12 -0500

    Delete docs para actulizar pagina
    Delete docs para actulizar pagina

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAlMjBrWcmzElyyczbseDmdAfEAL-_vxxI",
+    authDomain: "reservaciones-donmundo.firebaseapp.com",
+    databaseURL: "https://reservaciones-donmundo.firebaseio.com",
+    projectId: "reservaciones-donmundo",
+    storageBucket: "reservaciones-donmundo.appspot.com",
+    messagingSenderId: "547563065065",
+    appId: "1:547563065065:web:191f25e2c302375800aa78"
+},
 };
