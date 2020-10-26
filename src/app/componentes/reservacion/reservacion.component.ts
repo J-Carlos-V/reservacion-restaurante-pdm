8a430af979be1b3d2496e729fb09e2aa954c4098
Alejandrom-GO <miguel.a.gutierrezortega@gmail.com>
2020-10-26 14:12:53 -0600

    Modificacion de Validaciones
    Modificacion en las validaciones del formualrio de reservacion

--- src/app/componentes/reservacion/reservacion.component.ts
+++ src/app/componentes/reservacion/reservacion.component.ts
@@ -9,48 +9,65 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./reservacion.component.css']
 })
 export class ReservacionComponent implements OnInit {
+  public mesas: any = [];
+  public mesasDisponible: any = [];
+  public mesaReservada: any = {Mesas: ''};
   // tslint:disable-next-line:typedef
   private emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   // tslint:disable-next-line:typedef
   createFormGroup() {
     return new FormGroup({
-      firstname: new FormControl('', [Validators.required, Validators.minLength(5)]),
-      surname: new FormControl('', [Validators.required, Validators.minLength(10)]),
+      firstname: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      surname: new FormControl('', [Validators.required, Validators.minLength(8)]),
       email: new FormControl('', [Validators.required, Validators.minLength(5), Validators.pattern(this.emailPattern)]),
       number: new FormControl('', [Validators.required, Validators.maxLength(10), Validators.minLength(10)]),
       fecha: new FormControl('', [Validators.required]),
       hora: new FormControl('', [Validators.required]),
       personas: new FormControl('', [Validators.required, Validators.minLength(1)]),
       detalles: new FormControl('', [Validators.maxLength(100)]),
-      estado: new FormControl('Pendiente')
+      estado: new FormControl('Pendiente'),
+      mesa: new FormControl(Validators.required)
     });
   }
   // tslint:disable-next-line:member-ordering
   contacForm: FormGroup;
   constructor(private ser: ConService) {
     this.contacForm = this.createFormGroup();
+
+    this.ser.retornarMesas().subscribe((res: any) => {
+      this.mesas = res;
+      for (const mesa of this.mesas) {
+        if (mesa.estado === 'disponible') {
+          this.mesasDisponible.push(mesa);
+        }
+      }
+    });
+    console.log(this.mesasDisponible);
   }
-  ngOnInit(): void {
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
   }
 
-   // tslint:disable-next-line:typedef
-   get firstname() { return this.contacForm.get('firstname'); }
-   // tslint:disable-next-line:typedef
-   get surname() { return this.contacForm.get('surname'); }
-   // tslint:disable-next-line:typedef
-   get email() { return this.contacForm.get('email'); }
-   // tslint:disable-next-line:typedef
-   get number() { return this.contacForm.get('number'); }
-   // tslint:disable-next-line:typedef
-   get fecha() { return this.contacForm.get('fecha'); }
-   // tslint:disable-next-line:typedef
-   get hora() { return this.contacForm.get('hora'); }
-   // tslint:disable-next-line:typedef
-   get personas() { return this.contacForm.get('personas'); }
-   // tslint:disable-next-line:typedef
-   get detalles() { return this.contacForm.get('detalles'); }
-   // tslint:disable-next-line:typedef
-   get estado() { return this.contacForm.get('estado'); }
+  // tslint:disable-next-line:typedef
+  get firstname() { return this.contacForm.get('firstname'); }
+  // tslint:disable-next-line:typedef
+  get surname() { return this.contacForm.get('surname'); }
+  // tslint:disable-next-line:typedef
+  get email() { return this.contacForm.get('email'); }
+  // tslint:disable-next-line:typedef
+  get number() { return this.contacForm.get('number'); }
+  // tslint:disable-next-line:typedef
+  get fecha() { return this.contacForm.get('fecha'); }
+  // tslint:disable-next-line:typedef
+  get hora() { return this.contacForm.get('hora'); }
+  // tslint:disable-next-line:typedef
+  get personas() { return this.contacForm.get('personas'); }
+  // tslint:disable-next-line:typedef
+  get detalles() { return this.contacForm.get('detalles'); }
+  // tslint:disable-next-line:typedef
+  get estado() { return this.contacForm.get('estado'); }
+  // tslint:disable-next-line:typedef
+  get mesa() { return this.contacForm.get('mesa'); }
 
   // tslint:disable-next-line:typedef
   onResetForm() {
@@ -60,11 +77,17 @@ export class ReservacionComponent implements OnInit {
   agregar() {
     if (this.contacForm.valid) {
       this.ser.addItem(this.contacForm.value);
+      this.editarmesa(this.contacForm.value);
       this.onResetForm();
       window.alert('Reservacion realizada con exito, Si necesitas cancelar llama a este número 764 764 3040');
     } else {
       window.alert('Verifica tus datos ocurrio un error');
     }
+  }
+
+  // tslint:disable-next-line:typedef
+  editarmesa(mesa){
+    console.log(mesa);
   }
 
 }
--- src/app/services/conexion.service.ts
+++ src/app/services/conexion.service.ts
@@ -1,25 +1,47 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 export interface Item { name: string; }
-
+export interface Mesas {name: string;}
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class ConService {
   private itemsCollection: AngularFirestoreCollection<Item>;
   items: Observable<Item[]>;
-  constructor(private afs: AngularFirestore) {
+   private MesasCollection: AngularFirestoreCollection<Mesas>;
+  mesas: Observable<Mesas[]>;
+  constructor(private readonly afs: AngularFirestore) {
     this.itemsCollection = afs.collection<Item>('items');
     this.items = this.itemsCollection.valueChanges();
+    this.MesasCollection = afs.collection<Mesas>('mesas');
+    this.mesas = this.MesasCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Item;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
+    // this.mesas = this.MesasCollection.valueChanges();
 }
 // tslint:disable-next-line:typedef
 retornarItems(){
   return this.items;
 }
+// tslint:disable-next-line:typedef
+retornarMesas(){
+  return this.mesas;
+  // return this.afs.collection('mesas').snapshotChanges;
+  // return this.afs.collection('mesas').doc('5nBEvdb8t7pU2MCY5Ntb').snapshotChanges;
+}
 // tslint:disable-next-line: typedef
 addItem(item: Item) {
   this.itemsCollection.add(item);
+}
+// tslint:disable-next-line:typedef
+editarmesa(){
 }
 }
